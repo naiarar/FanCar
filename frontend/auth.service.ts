4f8ca21a99714b6c30c3476994d2970a26a743d8
Naiara Rodrigues <rnaiara92@gmail.com>
2023-07-14 10:25:27 -0300

    autenticação jwt angular

---
+++ frontend/auth.service.ts
@@ -0,0 +1,18 @@
+// auth.service.ts
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    // Lógica de autenticação
+  }
+
+  // Outros métodos relacionados à autenticação (logout, etc.)
+}
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { HomeComponent } from './home.component';
+import { ProfileComponent } from './profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [  
+{ path: '', component: HomeComponent },
+{ path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
