c31fb1ebadf09b2d09a4e44437874db17f574ea3
Naiara Rodrigues <rnaiara92@gmail.com>
2023-07-17 09:59:27 -0300

    urls jwt

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import CustomTokenObtainPairView, CustomTokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from rest_framework import routers
 from carros.api import viewsets as carrosviewset
 from django.conf.urls.static import static
@@ -13,8 +16,8 @@ route.register(r'carros', carrosviewset.CarrosViewset, basename= "Carros")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(route.urls)),
-    path('api/token/', CustomTokenObtainPairView.as_view()),
-    path('api/token/refresh/', CustomTokenRefreshView.as_view())
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
