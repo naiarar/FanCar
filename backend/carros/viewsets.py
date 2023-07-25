c70bacc8b92bbc82a4004651b83a227b09523d71
Naiara Rodrigues <rnaiara92@gmail.com>
2023-07-25 13:21:06 -0300

    ordenação por valor

--- backend/carros/viewsets.py
+++ backend/carros/viewsets.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 from carros import serializers
 from carros import models
+from rest_framework.filters import OrderingFilter
+
 
 class CarrosViewset(viewsets.ModelViewSet):
     serializer_class = serializers.CarrosSerializer
     queryset = models.Carros.objects.all()
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['valor']
