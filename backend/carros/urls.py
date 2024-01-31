dd42f59a95494127a6a9c1cd90aa771a051bc597
naiarar <rnaiara92@gmail.com>
2024-01-31 12:04:31 -0300

    criando nova rota pra atualizar carro

--- backend/carros/urls.py
+++ backend/carros/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CarrosListCreate, CarrosRetrieveUpdateDelete
+from .views import CarrosListCreate, CarrosRetrieveUpdateDelete, CarrosUpdateView
 
 urlpatterns = [
     path('carros', CarrosListCreate.as_view(), name='Criar-Lista-Carros '),
-    path('carros/<int:pk>/', CarrosRetrieveUpdateDelete.as_view(), name='carros-detalhes')
+    path('carros/<int:pk>/', CarrosRetrieveUpdateDelete.as_view(), name='carros-detalhes'),
+    path('carros/<int:pk>/update/', CarrosUpdateView.as_view(), name='carros-update'),
 ]
